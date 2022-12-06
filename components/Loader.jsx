339c30d49d05d4a308af7b54e1bbf91eb22a286d
Connor Belk <connor.belk@gmail.com>
2022-12-05 21:53:49 -0900

    Loader fixed, next fix will be display

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-import Image from "next/image";
 
 const Loader = () => {
-  return (
-    <>
-      <Image classname="w-[200px] m-auto block" src={null} alt="Loading..." />
-    </>
-  );
+  return <div id="loader"></div>;
 };
 
 export default Loader;
